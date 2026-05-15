aa5712f6de4d8e7e00bb5b395f2d635b2b75bd6d
RADHAji <ujjuarora5@gmail.com>
2026-05-16 01:45:45 +0530

    final clean backend ready

--- server.js
+++ server.js
@@ -194,14 +194,25 @@ app.post("/place-order", async (req, res) => {
       });
     }
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL_USER,
+    //     pass: process.env.EMAIL_PASS,
+    //   },
+    // });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
     const orderDetails = cartItems
       .map(
         (item) =>
