391d1909f95b6fb16cc1fec3bbb146af2686625e
RADHAji <ujjuarora5@gmail.com>
2026-05-17 14:47:28 +0530

    final clean backend ready

--- server.js
+++ server.js
@@ -272,8 +272,15 @@ app.post("/create-razorpay-order", async (req, res) => {
   try {
     const { amount } = req.body;
 
+    if (!amount) {
+      return res.status(400).json({
+        success: false,
+        message: "Amount is required",
+      });
+    }
+
     const options = {
-      amount: amount * 100, // paise
+      amount: Number(amount) * 100,
       currency: "INR",
       receipt: `receipt_${Date.now()}`,
     };
@@ -290,13 +297,13 @@ app.post("/create-razorpay-order", async (req, res) => {
 
   } catch (error) {
     console.log("RAZORPAY ORDER ERROR:", error);
+
     res.status(500).json({
       success: false,
-      message: "Payment initialization failed",
+      message: error.message,
     });
   }
 });
-
 /* =========================
    VERIFY RAZORPAY PAYMENT
 ========================= */
