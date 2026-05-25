6fe7e6c190ad105a61f4d53698a40309c2139caf
RADHAji <ujjuarora5@gmail.com>
2026-05-25 14:35:59 +0530

    final clean backend ready

--- server.js
+++ server.js
@@ -846,8 +846,8 @@ app.post("/saveuser", (req, res) => {
 
 });
 
-let dailyBillCounter = 0;
-let lastBillDate = "";
+// let dailyBillCounter = 0;
+// let lastBillDate = "";
 
 app.post("/place-order", async (req, res) => {
   try {
