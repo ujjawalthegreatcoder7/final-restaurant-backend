0baa32852c95fe88adbfcb3993426df75c42a36b
RADHAji <ujjuarora5@gmail.com>
2026-05-25 14:32:42 +0530

    final clean backend ready

--- server.js
+++ server.js
@@ -876,7 +876,7 @@ app.post("/place-order", async (req, res) => {
        BILL NUMBER (DAILY RESET)
     ========================= */
     let dailyBillCounter = 0;
-    let lastBillDate = new Date().toDateString();
+let lastBillDate = new Date().toDateString();
     const today = new Date().toDateString();
 
     if (today !== lastBillDate) {
